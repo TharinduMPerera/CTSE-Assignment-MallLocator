049820a5fdc477344c9e14d7499df64da93e9180
Tharindu <tharindu.madushanka@my.sliit.lk>
2018-03-21 23:02:15 +0530

    - Splash screen done - Home screen init

--- app/src/main/java/com/tharinduapps/malllocator/activities/MainActivity.java
+++ app/src/main/java/com/tharinduapps/malllocator/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tharinduapps.malllocator;
+package com.tharinduapps.malllocator.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tharinduapps.malllocator.R;
 
 public class MainActivity extends AppCompatActivity {
 
