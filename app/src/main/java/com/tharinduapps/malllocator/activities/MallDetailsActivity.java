8d5f706f9ae7e7acc5b5b0740e2a1b0a1e25e7f7
Tharindu <tharindu.madushanka@my.sliit.lk>
2018-03-30 12:08:56 +0530

    - my part is done

--- app/src/main/java/com/tharinduapps/malllocator/activities/MallDetailsActivity.java
+++ app/src/main/java/com/tharinduapps/malllocator/activities/MallDetailsActivity.java
@@ -4,17 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.tharinduapps.malllocator.R;
 import com.tharinduapps.malllocator.models.Mall;
 
-public class MallDetailsActivity extends AppCompatActivity {
+public class MallDetailsActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private ImageButton backBtn;
     private Mall mall;
     private TextView title;
+    private TextView contactTextView;
+    private TextView locationTextView;
+    private ImageView coverImage;
+    private RatingBar ratingBar;
+
+//    private View mapView;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,7 @@ public class MallDetailsActivity extends AppCompatActivity {
         initUI();
         setObject();
         setUI();
+        initMap();
     }
 
     private void initUI(){
@@ -34,6 +51,10 @@ public class MallDetailsActivity extends AppCompatActivity {
     private void initElements(){
         backBtn = (ImageButton) findViewById(R.id.backBtn);
         title = (TextView) findViewById(R.id.title);
+        contactTextView = (TextView) findViewById(R.id.contactTextView);
+        locationTextView = (TextView) findViewById(R.id.locationTextView);
+        coverImage = (ImageView) findViewById(R.id.coverImage);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
     }
 
     private void initOnClicks(){
@@ -46,6 +67,13 @@ public class MallDetailsActivity extends AppCompatActivity {
         });
     }
 
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+//        mapView = mapFragment.getView();
+    }
+
     private void setObject() {
         Object object = getIntent().getExtras().getParcelable("mall");
         if (object!=null){
@@ -56,6 +84,24 @@ public class MallDetailsActivity extends AppCompatActivity {
     private void setUI(){
         if(mall!=null) {
             title.setText(mall.getName());
+            contactTextView.setText("Contact: "+mall.getTelephone());
+            locationTextView.setText("Location: "+mall.getCity());
+            ratingBar.setRating(mall.getRate());
+            coverImage.setImageResource(getResources().getIdentifier(mall.getCoverImage(),"drawable",getPackageName()));
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        if(mall!=null){
+            LatLng latLog = new LatLng(mall.getLat(), mall.getLon());
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLog, 15.0f));
+
+            map.addMarker(new MarkerOptions()
+                    .position(latLog)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
         }
     }
 }
--- app/src/main/java/com/tharinduapps/malllocator/activities/SettingsActivity.java
+++ app/src/main/java/com/tharinduapps/malllocator/activities/SettingsActivity.java
@@ -113,16 +113,16 @@ public class SettingsActivity extends AppCompatActivity {
     private ArrayList<Mall> getMallObjects(){
         ArrayList<Mall> malls = new ArrayList<>();
 
-        malls.add(new Mall(1,"image1","Liberty Plaza","Colombo 3","0112 254 811","Liberty_Plaza_Cover",4.2f,6.9113598,79.851157));
-        malls.add(new Mall(2,"image2","Majestic City","Colombo 4","0112 508 673","Majestic_City_Cover",4.3f,6.894127,79.8546198));
-        malls.add(new Mall(3,"image3","Arcade Independence Square","Colombo","071 345 6789","Liberty_Plaza_Cover",4.2f,6.902570,79.868915));
-        malls.add(new Mall(4,"image4","Dutch Hospital","Colombo 7","078 555 467","Dutch_Hospital_Cover",4.1f,6.902551,79.868832));
-        malls.add(new Mall(5,"image5","Crescat Shopping Mall","Colombo","0112 152 100","Crescat_Shopping_Mall_Cover",3.6f,6.917006,79.848256));
-        malls.add(new Mall(6,"image6","Colombo City Centre","Colombo","0112 300 100","Colombo_City Centre_Cover",5.0f,6.917609,79.855325));
-        malls.add(new Mall(7,"image7","Odel","Dehiwala-Mount Lavinia","0112 789 700","Odel_Cover",4.6f,6.841328,79.866775));
-        malls.add(new Mall(8,"image8","Cotton Collection","Colombo 3","0112 657 890","Cotton_Collection_Cover",3.6f,6.914775,79.859064));
-        malls.add(new Mall(9,"image9","Capital Mall","Pitakotte","0112 786 876","Capital_Mall_Cover",4.0f,6.874732,79.934653));
-        malls.add(new Mall(10,"image10","The Factory Outlet","Sri Jayawardenepura Kotte","0114 154 700","The_Factory_Outlet_Cover",4.5f,6.889142,79.929297));
+        malls.add(new Mall(1,"image1","Liberty Plaza","Colombo 3","0112 254 811","liberty_plaza_cover",4.2f,6.9113598,79.851157));
+        malls.add(new Mall(2,"image2","Majestic City","Colombo 4","0112 508 673","majestic_city_cover",4.3f,6.894127,79.8546198));
+        malls.add(new Mall(3,"image3","Arcade Independence Square","Colombo","071 345 6789","arcade_independence_square_cover",4.2f,6.902570,79.868915));
+        malls.add(new Mall(4,"image4","Dutch Hospital","Colombo 7","078 555 467","dutch_hospital_cover",4.1f,6.902551,79.868832));
+        malls.add(new Mall(5,"image5","Crescat Shopping Mall","Colombo","0112 152 100","crescat_shopping_mall_cover",3.6f,6.917006,79.848256));
+        malls.add(new Mall(6,"image6","Colombo City Centre","Colombo","0112 300 100","colombo_city_centre_cover",5.0f,6.917609,79.855325));
+        malls.add(new Mall(7,"image7","Odel","Dehiwala-Mount Lavinia","0112 789 700","odel_cover",4.6f,6.841328,79.866775));
+        malls.add(new Mall(8,"image8","Cotton Collection","Colombo 3","0112 657 890","cotton_collection_cover",3.6f,6.914775,79.859064));
+        malls.add(new Mall(9,"image9","Capital Mall","Pitakotte","0112 786 876","capital_mall_cover",4.0f,6.874732,79.934653));
+        malls.add(new Mall(10,"image10","The Factory Outlet","Sri Jayawardenepura Kotte","0114 154 700","the_factory_outlet_cover",4.5f,6.889142,79.929297));
 
         return  malls;
     }
