ddc219804e79020d17f002583e0c8a76a7b56bb6
Tharindu <tharindu.madushanka@my.sliit.lk>
2018-04-08 03:18:44 +0530

    - removed an unused package

--- app/src/main/java/com/tharinduapps/malllocator/activities/MallDetailsActivity.java
+++ app/src/main/java/com/tharinduapps/malllocator/activities/MallDetailsActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
